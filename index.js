9d51879c2eaac3ed40d5a8f9595c06b439ecda78
Nathan Cuevas <nathanacuevas97@gmail.com>
2023-06-09 12:42:09 -0700

    Figured out base structure of the CMS Helper Class

--- index.js
+++ index.js
@@ -1,31 +1,4 @@
-const inquirer = require('inquirer');
-
-async function init() {
-  const answers = await inquirer.prompt([
-    {
-      type: 'list',
-      name: 'init',
-      message: 'What would you like to do?',
-      choices: [
-        'View All Employees',
-        'View All Employees By Department',
-        'View All Employees By Manager',
-        'View All Departments',
-        'View All Roles',
-        'Add Employee',
-        'Add Role',
-        'Add Department',
-        'Remove Department',
-        'Remove Employee',
-        'Remove Role',
-        'Update Employee Role',
-        'Update Employee Manager',
-        'Quit',
-      ],
-    },
-  ]);
-  console.log(answers);
-}
+const CMS = require('./utils/CMS');
 
 (async () => {
   console.log(`
@@ -34,17 +7,19 @@ Welcome to Your Employee Tracker
 \x1b[36m ==============================\x1b[0m
 `);
 
-  await init();
+  const db = new CMS();
 
-  await inquirer
-    .prompt([
-      {
-        type: 'input',
-        name: 'name',
-        message: 'What is your name?',
-      },
-    ])
-    .then((answers) => {
-      console.log(answers);
-    });
+  db.init();
+
+  // await inquirer
+  //   .prompt([
+  //     {
+  //       type: 'input',
+  //       name: 'name',
+  //       message: 'What is your name?',
+  //     },
+  //   ])
+  //   .then((answers) => {
+  //     console.log(answers);
+  //   });
 })();
--- utils/CMS.js
+++ utils/CMS.js
@@ -1,6 +1,64 @@
+const inquirer = require('inquirer');
+
 class CMS {
   constructor() {
-
+    this.db = require('../config/connection');
   }
-  
+  async 'View All Employees'() {
+    this.db
+      .promise()
+      .query('SELECT * FROM employee')
+      .then((results) => console.table(results[0]))
+      .finally(async () => {
+        await this.init();
+      })
+      .catch((err) => {
+        throw err;
+      });
+  }
+  'View All Employees By Department'() {}
+  'View All Employees By Manager'() {}
+  'View All Departments'() {}
+  'View All Roles'() {}
+  'Add Employee'() {}
+  'Add Role'() {}
+  'Add Department'() {}
+  'Remove Department'() {}
+  'Remove Employee'() {}
+  'Remove Role'() {}
+  'Update Employee Role'() {}
+  'Update Employee Manager'() {}
+  Quit() {
+    console.log('Goodbye!');
+    process.exit();
+  }
+  async init() {
+    const answers = await inquirer.prompt([
+      {
+        type: 'list',
+        name: 'init',
+        message: 'What would you like to do?',
+        choices: [
+          'View All Employees',
+          'View All Employees By Department',
+          'View All Employees By Manager',
+          'View All Departments',
+          'View All Roles',
+          'Add Employee',
+          'Add Role',
+          'Add Department',
+          'Remove Department',
+          'Remove Employee',
+          'Remove Role',
+          'Update Employee Role',
+          'Update Employee Manager',
+          'Quit',
+        ],
+      },
+    ]);
+    console.log(answers);
+    this[answers.init]();
+  }
 }
+
+module.exports = CMS;
